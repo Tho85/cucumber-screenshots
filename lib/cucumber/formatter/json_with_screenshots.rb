f09dcb49ba009e63c9aa692f74e03a170b6022cf
Steffen Schildknecht <steffen.schildknecht@zweitag.de>
2011-08-26 10:09:53 +0200

    remove ActionMailer dependency

--- lib/cucumber/formatter/json_with_screenshots.rb
+++ lib/cucumber/formatter/json_with_screenshots.rb
@@ -81,9 +81,9 @@ module Cucumber
       end
 
       def emails_for_step(step)
-        if (defined? ActionMailer)
-          ActionMailer::Base.deliveries.collect{|mail| mail_as_json(mail) }
-        end
+        #if (defined? ActionMailer)
+          #ActionMailer::Base.deliveries.collect{|mail| mail_as_json(mail) }
+        #end
       end
 
       def mail_as_json(mail)
